0f92519be8e6de3181066e420f8bf16599acd94d
Sam Mikes <smikes@cubane.com>
2014-06-06 21:12:23 +0100

    constructor: initial tests of Promise constructor
    Add change to fix redundancy noted by @dilvie

---
+++ test/constructor.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var assert = require("assert"),
+    path = require("path");
+require(path.join(__dirname,"../lib/npo.src.js"));
+
+describe("Promise constructor", function () {
+    it("is provided", function () {
+	assert.equal(typeof Promise,"function");
+    });
+
+    it("returns a new Promise", function () {
+	var p = new Promise(function () {});
+
+	assert.ok(p instanceof Promise);
+    });
+});
