78f4f60a87642fd01a5da93f080f2c8033207171
smikes <smikes@cubane.com>
2014-06-06 17:51:08 -0600

    test: remove redundant line

--- test/constructor.js
+++ test/constructor.js
@@ -6,7 +6,6 @@ require(path.join(__dirname,"../lib/npo.src.js"));
 
 describe("Promise constructor", function () {
     it("is provided", function () {
-	assert.notEqual(Promise,undefined);
 	assert.equal(typeof Promise,"function");
     });
 
