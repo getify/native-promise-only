90b529e551107eadd6cca4fe0dbab973c0a6c488
Sam Mikes <smikes@cubane.com>
2014-06-07 14:49:05 +0100

    test: factor out helper functions

---
+++ test/helpers.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var assert = require('assert');
+
+module.exports.unexpectedResolve = function unexpectedResolve(resolved) {
+	throw "Unexpected resolve " + resolved;
+}
+
+module.exports.unexpectedReject = function unexpectedReject(err) { 
+	throw "Unexpected reject " + err;
+}
+
+module.exports.resolveImmediately = function resolveImmediately(arg) {
+	return function (resolve, reject) {
+		setImmediate(function () { resolve(arg); });
+	};
+}
+
+module.exports.rejectImmediately = function rejectImmediately(arg) {
+	return function (resolve, reject) {
+		setImmediate(function () { reject(arg); });
+	};
+}
+
+module.exports.resolveAfter = function resolveAfter(time, arg) {
+	return function (resolve, reject) {
+		setTimeout(function () { resolve(arg); }, time);
+	};
+}
+
+module.exports.rejectAfter = function rejectAfter(time, arg) {
+	return function (resolve, reject) {
+		setTimeout(function () { reject(arg); }, time);
+	};
+}
+
+module.exports.expectedResolve = function expectedResolve(obj, done) {
+	return function (resolved) {
+		assert.equal(obj, resolved);
+		done();
+	};
+}
+
+module.exports.expectedResolveDeep = function expectedResolveDeep(obj, done) {
+	return function (resolved) {
+		assert.deepEqual(obj, resolved);
+		done();
+	};
+}
+
+module.exports.expectedReject = function expectedReject(obj, done) {
+	return function (resolved) {
+		assert.equal(obj, resolved);
+		done();
+	};
+}
+
+module.exports.neverResolve = function neverResolve() {
+};
