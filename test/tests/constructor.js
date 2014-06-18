8caf7b0131e1845f810ad516e35ad7bc5555814f
smikes <smikes@cubane.com>
2014-06-18 16:18:22 -0600

    minor changes to tests, large whitespace chgs
    add catch(done) in constructor tests to catch errors
    queue p1 onResolved before p2
    formatting / indentation changes only

--- test/tests/constructor.js
+++ test/tests/constructor.js
@@ -4,7 +4,7 @@ var assert = require("assert");
 
 describe("25.4.3 The Promise Constructor", function () {
     it("is the initial value of the Promise property of the global object", function () {
-	assert.strictEqual(Promise, global.Promise);
+	    assert.strictEqual(Promise, global.Promise);
     });
 
     it("can be called as a function");
@@ -28,79 +28,80 @@ describe("25.4.3 The Promise Constructor", function () {
 });
 
 describe("25.4.3.1 Promise ( executor )", function () {
+
     it("throws TypeError when 'this' is not of type Object", function () {
-	assert.throws(function () {
-	    Promise.call(3, function () {});
-	}, TypeError);
+	    assert.throws(function () {
+	        Promise.call(3, function () {});
+	    }, TypeError);
     });
 
     it("throws TypeError if 'this' is a constructed, but unresolved Promise", function (done) {
-	var resolveP,
-	    p = new Promise(function (resolve, reject) { resolveP = resolve; });
+	    var resolveP,
+	        p = new Promise(function (resolve, reject) { resolveP = resolve; });
 
-	// promise's [[PromiseState]] internal slot should be 'pending'
-	// should throw
-	assert.throws(function () {
-	    Promise.call(p, function (resolve, reject) { resolve(2); });
-	}, TypeError);
-
-	// receive first resolution
-	p.then(function (resolved) {
-	    assert.equal(1, resolved);
-	}).then(done).catch(done);
-
-	resolveP(1);
-    });
-
-    it("throws TypeError if 'this' is a resolved Promise", function (done) {
-	var p = new Promise(function (resolve, reject) { resolve(1); });
-	
-	function afterFirstResolution() {
-	    // if promise's [[PromiseState]] internal slot is not 'undefined'
+	    // promise's [[PromiseState]] internal slot should be 'pending'
 	    // should throw
 	    assert.throws(function () {
-		Promise.call(p, function (resolve, reject) { resolve(2); });
+	        Promise.call(p, function (resolve, reject) { resolve(2); });
 	    }, TypeError);
-
-	    // affirm that previous resolution is still settled
+        
+	    // receive first resolution
 	    p.then(function (resolved) {
-		assert.equal(1, resolved);
+	        assert.equal(1, resolved);
 	    }).then(done).catch(done);
-	}
 
-	// receive first resolution
-	p.then(function (resolved) {
-	    assert.equal(resolved, 1);
+	    resolveP(1);
+    });
+    
+    it("throws TypeError if 'this' is a resolved Promise", function (done) {
+	    var p = new Promise(function (resolve, reject) { resolve(1); });
+	    
+	    function afterFirstResolution() {
+	        // if promise's [[PromiseState]] internal slot is not 'undefined'
+	        // should throw
+	        assert.throws(function () {
+		        Promise.call(p, function (resolve, reject) { resolve(2); });
+	        }, TypeError);
 
-	    Promise.resolve().then(afterFirstResolution);
-	});
+	        // affirm that previous resolution is still settled
+	        p.then(function (resolved) {
+		        assert.equal(1, resolved);
+	        }).then(done).catch(done);
+	    }
+
+	    // receive first resolution
+	    p.then(function (resolved) {
+	        assert.equal(resolved, 1);
+
+	        Promise.resolve().then(afterFirstResolution).catch(done);
+	    }).catch(done);
     });
 
     it("throws TypeError if 'executor' is not Callable", function () {
-	assert.throws(function () {
-	    new Promise("not callable");
-	}, TypeError);
+	    assert.throws(function () {
+	        new Promise("not callable");
+	    }, TypeError);
     });
 });
 
 describe("25.4.3.1.1 InitializePromise ( promise, executor )", function () {
     it("returns a promise");
     it("invokes the executor with 'this' = 'undefined'", function () {
-	var savedThis;
-	var p = new Promise(function () { savedThis = this; });
+	    var savedThis;
+	    var p = new Promise(function () { savedThis = this; });
 
-	assert.equal(undefined, savedThis);
+	    assert.equal(undefined, savedThis);
     });
     it("catches exceptions thrown from executor and turns them into reject", function (done) {
-	// if completion is an abrupt completion
-	var errorObject = {};
+	    // if completion is an abrupt completion
+	    var errorObject = {};
 
-	var p = new Promise(function () { throw errorObject; });
+	    var p = new Promise(function () { throw errorObject; });
 
-	p.then(undefined, function(err) {
-	    assert.equal(undefined, this);
-	    assert.equal(errorObject, err);
-	}).then(done).catch(done);
+	    p.then(undefined, function(err) {
+	        assert.equal(undefined, this);
+	        assert.equal(errorObject, err);
+	    }).then(done).catch(done);
 
     });
 
@@ -114,18 +115,18 @@ describe("25.4.3.2 new Promise ( ... argumentsList )", function () {
 describe("25.4.4 Properties of the Promise Constructor", function () {
     it("has a [[Protoype]] internal slot whose value is the Function prototype object");
     it("has a length property whose value is 1", function () {
-	assert.equal(1, Promise.length);
+	    assert.equal(1, Promise.length);
     });
 });
 
 describe("Promise constructor", function () {
     it("is provided", function () {
-	assert.equal(typeof Promise,"function");
+	    assert.equal(typeof Promise,"function");
     });
 
     it("returns a new Promise", function () {
-	var p = new Promise(function () {});
+	    var p = new Promise(function () {});
 
-	assert.ok(p instanceof Promise);
+	    assert.ok(p instanceof Promise);
     });
 });
